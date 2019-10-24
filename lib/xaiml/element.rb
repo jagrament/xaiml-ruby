c51ef931d1236228fd5e44f48c7f1d47e72a5396
Koji Yamazaki <kouji.yamazaki.cv@nttdocomo.com>
2019-10-24 16:47:11 +0900

    Added Tag <sraixtopic> and <botid>

--- lib/xaiml/element.rb
+++ lib/xaiml/element.rb
@@ -3,34 +3,36 @@
 module XAIML
   module Element
     autoload :Base, "xaiml/element/base"
-    class Arg       < Base; end
-    class Calc      < Base; end
-    class Command   < Base; end
-    class Get       < Base; end
-    class Loop      < Base; end
-    class Log       < Base; end
-    class Li        < Base; end
-    class Matcher   < Base; end
-    class Map       < Base; end
-    class Name      < Base; end
-    class Pattern   < Base; end
-    class Predstore < Base; end
-    class Set       < Base; end
-    class Srai      < Base; end
-    class Sraix     < Base; end
-    class Star      < Base; end
-    class Template  < Base; end
-    class That      < Base; end
-    class Think     < Base; end
-    class Value     < Base; end
-    class Ub        < Base; end
-    class Ut        < Base; end
-    class Utterance < Base; end
+    class Arg         < Base; end
+    class BotId       < Base; end
+    class Calc        < Base; end
+    class Command     < Base; end
+    class Get         < Base; end
+    class Loop        < Base; end
+    class Log         < Base; end
+    class Li          < Base; end
+    class Matcher     < Base; end
+    class Map         < Base; end
+    class Name        < Base; end
+    class Pattern     < Base; end
+    class Predstore   < Base; end
+    class Set         < Base; end
+    class Srai        < Base; end
+    class Sraix       < Base; end
+    class SraixTopic  < Base; end
+    class Star        < Base; end
+    class Template    < Base; end
+    class That        < Base; end
+    class Think       < Base; end
+    class Value       < Base; end
+    class Ub          < Base; end
+    class Ut          < Base; end
+    class Utterance   < Base; end
 
-    autoload :Category, "xaiml/element/category"
-    autoload :Condition, "xaiml/element/condition"
-    autoload :ConditionalRandom, "xaiml/element/conditional_random"
-    autoload :Ext, "xaiml/element/ext"
-    autoload :Topic, "xaiml/element/topic"
+    autoload :Category,           "xaiml/element/category"
+    autoload :Condition,          "xaiml/element/condition"
+    autoload :ConditionalRandom,  "xaiml/element/conditional_random"
+    autoload :Ext,                "xaiml/element/ext"
+    autoload :Topic,              "xaiml/element/topic"
   end
 end
